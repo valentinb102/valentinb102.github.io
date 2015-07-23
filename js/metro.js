4da3777f4da7a4d7344efb3868389599cb4510eb
valentinb102 <vben@live.com>
2015-07-22 21:33:57 -0400

    add js

---
+++ js/metro.js
@@ -0,0 +1,14 @@
+﻿
+; (function ($, document, window, undefined) {
+    $(function () {
+        // download and store metro data
+        var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + "http://web.mta.info/status/serviceStatus.txt" + '"') + '&format=xml&callback=?';
+
+        $.getJSON(yql, function (data) {
+            var serviceStatus = $.parseXML(data.results[0]);
+
+            // create an accordion for each line
+            console.log($(serviceStatus).find('line'));
+        });
+    });
+})(jQuery, document, window);
