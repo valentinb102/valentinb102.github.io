e28a56e005a9087e36dbbd3424fc3dd4fdc7ac8e
valentinb102 <vben@live.com>
2016-08-11 20:55:46 -0400

    adding tabs which will hold groups of lines

--- Scripts/metro.js
+++ Scripts/metro.js
@@ -1,49 +1,48 @@
 ﻿
-var panelTemplate;
-var trainLineColors = {
-    "ACE": {
-        "color": "#fff",
-        "background-color": "#2850AD"
-    },
-    "BDFM": {
-        "color": "#fff",
-        "background-color": "#FF6319"
-    },
-    "G": {
-        "color": "#fff",
-        "background-color": "#6CBE45"
-    },
-    "JZ": {
-        "color": "#fff",
-        "background-color": "#996633"
-    },
-    "L": {
-        "color": "#fff",
-        "background-color": "#A7A9AC"
-    },
-    "NQR": {
-        "color": "#000",
-        "background-color": "#FCCC0A"
-    },
-    "S": {
-        "color": "#fff",
-        "background-color": "#808183"
-    },
-    "123": {
-        "color": "#fff",
-        "background-color": "#EE352E"
-    },
-    "456": {
-        "color": "#fff",
-        "background-color": "#00933C"
-    },
-    "7": {
-        "color": "#fff",
-        "background-color": "#B933AD"
-    }
-};
-
 ; (function ($, document, window, undefined) {
+    var panelTemplate, trainLineColors = {
+        "ACE": {
+            "color": "#fff",
+            "background-color": "#2850AD"
+        },
+        "BDFM": {
+            "color": "#fff",
+            "background-color": "#FF6319"
+        },
+        "G": {
+            "color": "#fff",
+            "background-color": "#6CBE45"
+        },
+        "JZ": {
+            "color": "#fff",
+            "background-color": "#996633"
+        },
+        "L": {
+            "color": "#fff",
+            "background-color": "#A7A9AC"
+        },
+        "NQR": {
+            "color": "#000",
+            "background-color": "#FCCC0A"
+        },
+        "S": {
+            "color": "#fff",
+            "background-color": "#808183"
+        },
+        "123": {
+            "color": "#fff",
+            "background-color": "#EE352E"
+        },
+        "456": {
+            "color": "#fff",
+            "background-color": "#00933C"
+        },
+        "7": {
+            "color": "#fff",
+            "background-color": "#B933AD"
+        }
+    };
+
     $(function () {
         // save the panel template as clone
         panelTemplate = $(".panelTemplate").clone();
@@ -57,54 +56,52 @@ var trainLineColors = {
             updateServiceInfo(serviceStatus);
         });
     });
+
+    // update service info
+    function updateServiceInfo(serviceStatus) {
+        // set last updated date
+        $(".serviceLastUpdated").text("Last Update " + moment($(serviceStatus).find('timestamp').text(), "M/D/YYYY H:mm:ss A").fromNow());
+
+        console.log($("#metroTabs"))
+        //// create an accordion for each line
+        //$(serviceStatus).find('line').each(function (index, item) {
+        //    var newPanel = panelTemplate.clone();
+        //    var lineName = $.trim($(this).find('name').text());
+        //    var lineStatus = $.trim($(this).find('status').text());
+        //    var lineText = $.trim($(this).find('text').text());
+
+        //    // add line info
+        //    newPanel.find(".lineName").text(lineName);
+        //    newPanel.find(".lineStatus").text(lineStatus);
+
+        //    // add line color
+        //    if (trainLineColors[lineName]) {
+        //        newPanel.find(".lineName").css(trainLineColors[lineName]);
+        //    }
+
+        //    // add status class
+        //    if (lineStatus === "GOOD SERVICE") {
+        //        newPanel.find(".lineStatus").addClass("lineStatusGood");
+        //    } else {
+        //        newPanel.find(".lineStatus").addClass("lineStatusBad");
+        //    }
+
+        //    if (lineText.length) {
+        //        // add text as jquery element 
+        //        newPanel.find(".lineText").append($(lineText));
+
+        //        // add collapse support
+        //        newPanel.find(".panelLink").attr("href", "#collapse" + index);
+        //        newPanel.find(".panel-collapse").attr("id", "collapse" + index);
+        //    } else {
+        //        // disable collapse
+        //        newPanel.find(".panelLink").addClass("lineStatusGood").removeAttr("href").removeAttr("data-toggle");
+        //    }
+
+        //    $("#tabSubway").append(newPanel);
+        //});
+    }
 })(jQuery, document, window);
-
-// update service info
-function updateServiceInfo(serviceStatus) {
-    // set last updated date
-    $(".serviceLastUpdated").text("Last Update " + moment($(serviceStatus).find('timestamp').text(), "M/D/YYYY H:mm:ss A").fromNow());
-
-    // create an accordion for each line
-    $(serviceStatus).find('line').each(function (index, item) {
-        var newPanel = panelTemplate.clone();
-        var lineName = $.trim($(this).find('name').text());
-        var lineStatus = $.trim($(this).find('status').text());
-        var lineText = $.trim($(this).find('text').text());
-
-        // add line info
-        newPanel.find(".lineName").text(lineName);
-        newPanel.find(".lineStatus").text(lineStatus);
-
-        // add line color
-        if (trainLineColors[lineName]) {
-            newPanel.find(".lineName").css(trainLineColors[lineName]);
-        }
-
-        // add status class
-        if (lineStatus === "GOOD SERVICE") {
-            newPanel.find(".lineStatus").addClass("lineStatusGood");
-        } else {
-            newPanel.find(".lineStatus").addClass("lineStatusBad");
-        }
-
-        if (lineText.length) {
-            // add text as jquery element 
-            newPanel.find(".lineText").append($(lineText));
-
-            // add collapse support
-            newPanel.find(".panelLink").attr("href", "#collapse" + index);
-            newPanel.find(".panel-collapse").attr("id", "collapse" + index);
-        } else {
-            // disable collapse
-            newPanel.find(".panelLink").addClass("lineStatusGood").removeAttr("href").removeAttr("data-toggle");
-        }
-
-        // show the accordion
-        newPanel.removeClass("hidden");
-
-        $("#accordion").append(newPanel);
-    });
-}
 
 // mta specific stuff
 function ShowHide(id) {
