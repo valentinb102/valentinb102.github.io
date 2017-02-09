7db3ceb68117b163b7a9a98aa2461e76b39b2228
valentinb102 <vben@live.com>
2017-02-08 22:41:37 -0500

    github cant download the file

--- Scripts/metro.js
+++ Scripts/metro.js
@@ -59,14 +59,6 @@
                 updateServiceInfo(serviceStatus);
             }
         });
-
-        $.ajax({
-            method:"GET",
-            url: "http://web.mta.info/status/serviceStatus.txt",
-            success: function (data) {
-                console.log(data);
-            }
-        });
     });
 
     // update service info
